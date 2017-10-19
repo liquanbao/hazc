cfa49e0e1cf385d8824b3b72c9a238699a504f6b
liqb <liqb@hacbzc.com>
2017-10-19 10:04:15 +0800

    update 2017/10/19 10:04:15

--- a.bat
+++
@@ -1,2 +0,0 @@
-set THISTIME=%DATE:~0,10% %TIME:~0,8% update
-echo %THISTIME%
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
-set THISTIME=%DATE:~0,10% %TIME:~0,8% update
+set THISTIME=update %DATE:~0,10% %TIME:~0,8%
 git add .
 git commit -m "%THISTIME%"
 git push master master
